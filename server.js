f883ebe4ccb5004291beeab45cab6c0d40959485
Carlos Martinez <curlosmart@gmail.com>
2021-01-15 20:11:01 -0500

    Can input dates in format of '2015-12-25'

--- server.js
+++ server.js
@@ -24,9 +24,21 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/timestamp/:date?", function (req, res) {
+  //res.json({greeting: 'hello API'});
+  res.json({
+    unix: new Date(req.params.date).valueOf(),
+    utc: new Date(req.params.date)
+  });
+});
 
 // listen for requests :)
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port ' + 3000);
+});
+
+/*
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+*/
